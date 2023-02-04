5826e7987429bdae7700cc674ab5f32188de25a6
GavinStrikes <105064040+GavinCoded@users.noreply.github.com>
2023-02-04 10:37:05 -0500

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <windows.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+int main()
+{
+
+	cout << "Reverse Nord VPN Tool (test) \n";
+	std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+	cout << "Login not found, Redirecting you\n";
+	std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+	cout << "WIP\n";
+	std::this_thread::sleep_for(std::chrono::milliseconds(500000000));
+	return 0;
+	exit(0);
+
+
+}
